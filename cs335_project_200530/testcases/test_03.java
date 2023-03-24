939f6b985263b66574a8c4af7fedd0f73160c054
sandeepb20 <sandeepb20@iitk.ac.in>
2023-03-24 21:05:50 +0530

    adddd

--- cs335_project_200530/testcases/test_03.java
+++ cs335_project_200530/testcases/test_03.java
@@ -1,7 +1,5 @@
 public static public class test {
-    {
-        int l;
-    }
+    
     final int a = a+1;
     int c;
     int s; int p;
@@ -15,27 +13,13 @@ public static public class test {
         a = x;
         b = y;
         int z = 1;
-        for (int i = 0; i < 8; i++) {
-            a = x;
-            int z;
-            int f = z;
-            while (b > 9) {
-                int for2;
-            }
-            int z;
-        }
-        for (a = 0;; a++) {
-            int ab = 1;
-        }
+       
     }
 
     void add() {
         char e = a + b * d / c;
     }
 
-    {
-        char c = "z";
-    }
 }
 
 public class took {
