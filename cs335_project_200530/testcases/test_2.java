da4a700912810fa85dd8ae4c82564ef01438326d
kajaldeep <kajald20@iitk.ac.in>
2023-04-19 20:58:02 +0530

    arrayf

--- cs335_project_200530/testcases/test_2.java
+++ cs335_project_200530/testcases/test_2.java
@@ -2,7 +2,7 @@ public class test_1 {
     public int main(){
         // int a[] = new int[7];
         int b[][][] = new int[4][5][6];
-        int c = 4;
+        int c = b[1][2][3];
         b[1][2][3] = c;
     }
 }
